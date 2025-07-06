f637c0363cc0d74d594c5fd3b3991b77149b9b54
GeekyAsif786 <maniurrahaman1@gmail.com>
2025-07-07 02:28:58 +0530

    commmit all

---
+++ Arrays/2D Arrays/additionofmatrices.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    int r,c;
+    printf("Enter number of rows\n");
+    scanf("%d",&r);
+    printf("Enter number of columns\n");
+    scanf("%d",&c);
+    int arr[r][c];
+    int brr[r][c];
+    printf("Enter the elements of 1st array:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Enter the elements of 2nd array:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&brr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("The sum of the arrays:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){
+            printf("%d   ",(arr[i][j]+brr[i][j]));
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
