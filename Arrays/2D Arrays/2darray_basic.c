5ee7b4b438654e8809c38bba6bad56326f68258b
GeekyAsif786 <maniurrahaman1@gmail.com>
2025-07-07 01:19:30 +0530

    commit all

---
+++ Arrays/2D Arrays/2darray_basic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    int r,c;//int roll,marks;
+    printf("enter the number of students\n");
+    scanf("%d",&r);
+    printf("enter the number of subjects\n");
+    scanf("%d",&c);
+    int arr[r][c];
+    printf("Enter the roll and marks\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+            // printf("Enter the roll and marks\n");
+            // scanf("%d",&roll);
+            // scanf("%d",&marks);
+            // arr[i][j]={roll,marks};
+        }
+    }
+    printf("\n");
+    printf("The entered dataset is:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%d   ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
