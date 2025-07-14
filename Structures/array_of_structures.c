a70973241f8a6cae3b2ac63ea3d3b426a9e20718
GeekyAsif786 <maniurrahaman1@gmail.com>
2025-07-14 16:42:46 +0530

    updated/

--- Structures/array_of_structures.c
+++ Structures/array_of_structures.c
@@ -15,29 +15,57 @@ int main(){
     
     //taking input of attack of the 3 pokemons
 
-    printf("Enter the attack of- pikachu\ncharizard\ndialga\n");
+    printf("Enter the attack of-\npikachu\ncharizard\ndialga\n");
     for(int i=0;i<3;i++){
         scanf("%d",&arr[i].attack);
     }
     
     //taking input of hp of the 3 pokemons
 
-    printf("Enter the HP of- pikachu\ncharizard\ndialga\n");
+    printf("Enter the HP of-\npikachu\ncharizard\ndialga\n");
     for(int i=0;i<3;i++){
         scanf("%d",&arr[i].hp);
     }
     
     //taking input of defense of the 3 pokemons
 
-    printf("Enter the defense of- pikachu\ncharizard\ndialga\n");
+    printf("Enter the defense of-\npikachu\ncharizard\ndialga\n");
     for(int i=0;i<3;i++){
         scanf("%d",&arr[i].def);
     }
     
     //taking input of tier of the 3 pokemons
 
-    printf("Enter the tier of- pikachu\ncharizard\ndialga\n");
+    printf("Enter the tier of-\npikachu\ncharizard\ndialga\n");
     for(int i=0;i<3;i++){
         scanf("%c",&arr[i].tier);
     }
+    printf("\n");
+    printf("The attack of-\npikachu\ncharizard\ndialga\n");
+    for(int i=0;i<3;i++){
+        printf("%d\n",arr[i].attack);
+    }
+    
+    //taking input of hp of the 3 pokemons
+
+    printf("The HP of-\npikachu\ncharizard\ndialga\n");
+    for(int i=0;i<3;i++){
+        printf("%d\n",arr[i].hp);
+    }
+    
+    //taking input of defense of the 3 pokemons
+
+    printf("The defense of-\npikachu\ncharizard\ndialga\n");
+    for(int i=0;i<3;i++){
+        printf("%d\n",arr[i].def);
+    }
+    
+    //taking input of tier of the 3 pokemons
+
+    printf("The tier of-\npikachu\ncharizard\ndialga\n");
+    for(int i=0;i<3;i++){
+        printf("%c\n",arr[i].tier);
+    }
+
+    return 0;
 }
